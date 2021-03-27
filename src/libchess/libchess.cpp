938fe42edd85de8cb66f8684ab6e4614795fab4e
Andrew Nikonorov <andreynikonorov5@gmail.com>
2021-03-27 01:15:31 -0700

    Add function figure_check

--- src/libchess/libchess.cpp
+++ src/libchess/libchess.cpp
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+bool figure_check(char table[8][8], int y, int x, char input)
+{ // Проверка типа фигуры
+    if ((input + 32) == table[y][x] || input == table[y][x])
+        return true;
+    else
+        return false;
+}
+
 void print_board(char table[8][8])
 { // вывод доски
     for (int i = 0; i < 8; ++i) {
--- src/libchess/libchess.h
+++ src/libchess/libchess.h
@@ -1,1 +1,2 @@
 void print_board(char table[8][8]);
+bool figure_check(char table[8][8], int y, int x, char input);
