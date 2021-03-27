177ea62d581f4b9d58edc732c4c361636e567cff
Andrew Nikonorov <andreynikonorov5@gmail.com>
2021-03-27 00:58:51 -0700

    Implemented board output

---
+++ src/chess/main.cpp
@@ -0,0 +1,37 @@
+#include <clocale>
+#include <libchess/libchess.h>
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	
+	char table[8][8]; //объявление доски
+	
+    for (int i = 0; i < 8; ++i) //заполнение доски
+        for (int j = 0; j < 8; ++j)
+            table[i][j] = ' '; 
+            
+    for (int i = 0; i < 8; ++i) {
+        table[1][i] = 'p';
+        table[6][i] = 'P';
+    }
+    
+    table[0][0] = 'r';
+    table[0][7] = 'r';
+    table[7][0] = 'R';
+    table[7][7] = 'R';
+    table[0][1] = 'n';
+    table[0][6] = 'n';
+    table[7][1] = 'N';
+    table[7][6] = 'N';
+    table[0][2] = 'b';
+    table[0][5] = 'b';
+    table[7][2] = 'B';
+    table[7][5] = 'B';
+    table[0][3] = 'q';
+    table[7][3] = 'Q';
+    table[0][4] = 'k';
+    table[7][4] = 'K';
+
+    
+    print_board(table);
+}
